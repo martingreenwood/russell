00f363ba9780cefec28ab758c53d5d795bfa885a
Martin Greenwood <martin@pixelpudu.com>
2016-05-17 16:44:16 +0100

    404 edit

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,23 +12,6 @@
 <section class="no-results not-found">
 
 	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'russell' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'russell' ); ?></p>
-			<?php
-				get_search_form();
-
-		else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'russell' ); ?></p>
-			<?php
-				get_search_form();
-
-		endif; ?>
+		Mothing to see here.
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
