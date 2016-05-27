e70634a39e6aa7f55f08d6d01e91fa575e647040
Martin Greenwood <martin@pixelpudu.com>
2016-05-27 14:13:22 +0100

    added likes

--- single-developments.php
+++ single-developments.php
@@ -66,6 +66,11 @@ get_header(); ?>
 				if ( function_exists( 'sharing_display' ) ) {
 					sharing_display( '', true );
 				}
+
+				if ( class_exists( 'Jetpack_Likes' ) ) {
+					$custom_likes = new Jetpack_Likes;
+					echo $custom_likes->post_likes( '' );
+				}
 				?>
 			</div>
 
